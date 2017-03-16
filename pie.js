82d01d82b3c35d0c69c51c6a2f2a7347a93569f6
Recidvst <cdsnowden@gmail.com>
2017-03-16 22:45:44 +0000

    looping json and updated with colours

--- pie.js
+++ pie.js
@@ -1,24 +1,20 @@
 var canvas = document.getElementById('chartcanvas');
 var ctx = canvas.getContext('2d');
 
-var slices = [80,50,25,60,90];
 var total = 0;
-var color = ['#9aefdd','#dd729e','#6767ea','#ebca7d','#e18769'];
+// var color = ['#9aefdd','#dd729e','#6767ea','#ebca7d','#e18769'];
 var angle = 1.5*Math.PI;
 
-for(var e = 0; e < slices.length; e++)
-{
-	total += slices[e];
-	$('#total-label').text("Total: " + total);
-}
-
-for (var i = 0; i < slices.length; i++) {
-	ctx.fillStyle = color[i];
-	ctx.beginPath();
-	ctx.arc(canvas.width/2,canvas.height/2,canvas.height/2,angle,angle+(Math.PI*2*(slices[i]/total)));
-	ctx.lineTo(canvas.width/2,canvas.height/2);
-	ctx.fill();
-	angle += Math.PI*2*(slices[i]/total);
-  // ctx.fillText(color[i], canvas.width/2, angle);
-}
-
+$.getJSON( "https://raw.githubusercontent.com/Recidvst/canvas-pie-chart/master/company-times.json", function(jsondata) {
+	$.each(jsondata, function(key, data) {
+		var num = Number(data.current);
+		total += num;
+		$('#total-label').text("Total: " + total);
+		ctx.fillStyle = data.colour;
+		ctx.beginPath();
+		ctx.arc(canvas.width/2,canvas.height/2,canvas.height/2,angle,angle+(Math.PI*2*(num/total)));
+		ctx.lineTo(canvas.width/2,canvas.height/2);
+		ctx.fill();
+		angle += Math.PI*2*(num/total);
+	});
+});
