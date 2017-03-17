6a0aa0d571f6a868224f46e0423eadb89d82697d
Recidvst <cdsnowden@gmail.com>
2017-03-17 22:29:22 +0000

    adding json count and test json

--- pie.js
+++ pie.js
@@ -2,13 +2,13 @@ var canvas = document.getElementById('chartcanvas');
 var ctx = canvas.getContext('2d');
 
 var total = 0;
-var angle = 1.5*Math.PI;
+var angle = 0;
 var list = "";
 
 $.getJSON( "https://raw.githubusercontent.com/Recidvst/canvas-pie-chart/master/company-times.json", function(jsondata) {
+	var count = Object.keys(jsondata).length;
 	$.each(jsondata, function(key, data) {
 		var num = Number(data.current);
-		console.log(num)
 		total += num;
 		list += "<li>" + key + " = " + data.current + "</li>"
 		$('#total-label').text("Total: " + total);
@@ -20,6 +20,6 @@ $.getJSON( "https://raw.githubusercontent.com/Recidvst/canvas-pie-chart/master/c
 		ctx.lineTo(canvas.width/2,canvas.height/2);
     ctx.stroke();
 		ctx.fill();
-		angle += Math.PI*2*(num/total);
+		angle += Math.PI*2*(num/count);
 	});
 });
